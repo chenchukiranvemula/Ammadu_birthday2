f2c9f0b8297070316627460942ed8113bdd11fc8
CHENCHUKIRAN <chenchukiranvemula@gmail.com>
2026-07-23 00:44:45 +0530

    Fix celebration start condition for chapter 9

--- Script.js
+++ Script.js
@@ -62,10 +62,11 @@ nextBtns.forEach(btn => {
             bgMusic.play().catch(() => {});
             musicBtn.innerHTML = "▶ Play Our Song";
         }
-if(currentChapter === 9){
-    startCelebration();
-}
+
         chapters[currentChapter].classList.remove("active");
+        if(currentChapter === 9){
+    startCelebration();
+        }
         // Start typing when Chapter 8 opens
 if(currentChapter === 7){
     setTimeout(startLetterTyping,500);
