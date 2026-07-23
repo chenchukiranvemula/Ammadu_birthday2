d642e1b07a7ecd3191f0e407119adce74147be9a
CHENCHUKIRAN <chenchukiranvemula@gmail.com>
2026-07-23 10:45:00 +0530

    Refactor attempt handling in Script.js

--- Script.js
+++ Script.js
@@ -798,9 +798,8 @@ duration:450
 
 });
 
-if(attempts<=0){
-    unlockBtn.disabled = true;
-    timerArea.innerHTML = "Too many attempts. Refresh the page to try again.";
+if (attempts <= 0) {
+    startTimer();
 }
     
 }
